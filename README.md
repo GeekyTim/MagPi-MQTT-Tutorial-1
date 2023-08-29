296f8884631f8dcb9480b738cc3e44aeed95b484
Tim Richardson <software@potton.me.uk>
2023-08-29 16:31:45 +0100

    MQTT Publish

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MagPi-MQTT-Tutorial-1
 
-Simple code for publishing and subscribing to MQTT topics.
+Code for the MagPi article in the September 2023 issue of MagPi.
---
+++ mqtt_publish.py
@@ -0,0 +1,11 @@
+# Import the paho-mqtt client library.
+import paho.mqtt.client as mqtt
+
+# Create an MQTT Client – the client ID must be unique
+client = mqtt.Client("Sender")
+
+# Connect the client
+client.connect("localhost")
+
+# Publish a message to the MyHouse/garage/temperature topic
+client.publish("MyHouse/garage/temperature", "22.0")
